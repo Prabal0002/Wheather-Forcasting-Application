1b590593027effc363a011772cabc323c65d2886
Prabal Singh Bhadoriya <129841865+Prabal0002@users.noreply.github.com>
2023-08-08 22:25:38 +0530

    Update apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "5bafa9ecd0bf785de1164d29296b5428",
   base: "https://api.openweathermap.org/data/2.5/",
 };
